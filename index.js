4684f7f3a106c731fb19b00f38c38d29b722655a
Abdullah-git-hub <arafat200423@gmail.com>
2024-05-09 16:32:29 +0600

    rrrrrrrrrrrrrrrrrr

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 // const { connectToDB, getDB } = require("./db");
-const { ShortUrl } = require("../db");
-const { makeid } = require("../id");
+const { ShortUrl } = require("./db");
+const { makeid } = require("./id");
 const mongoose = require("mongoose");
 
 const app = express();
