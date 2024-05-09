0b9fd10b2468ed13c0fe2af84572fb32f6db8931
Abdullah-git-hub <arafat200423@gmail.com>
2024-05-09 17:10:11 +0600

    x

--- routes/url.js
+++ routes/url.js
@@ -8,10 +8,9 @@ const {
 
 const router = express.Router();
 
-router.get("/", showIndexHtmlPage);
-
-router.post("/longurl", postLongUrl);
-
-router.get("/:urlId", getRedirectedUrl);
+router
+    .get("/", showIndexHtmlPage)
+    .post("/longurl", postLongUrl)
+    .get("/:urlId", getRedirectedUrl);
 
 module.exports = router;
