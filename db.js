8d72b98b32e94f9b13e0707d819635ff345a883e
Abdullah-git-hub <arafat200423@gmail.com>
2024-05-09 15:50:13 +0600

    added mongoose

--- db.js
+++ db.js
@@ -1,19 +1,40 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 
-let dbConnection;
+const urlSchema = new mongoose.Schema(
+    {
+        url: {
+            type: String,
+            required: true,
+        },
+        urlHash: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+    },
+    { timestamps: true }
+);
 
-const connectToDB = (connection_string, callback) => {
-    MongoClient.connect(connection_string)
-        .then((client) => {
-            dbConnection = client.db();
-            return callback();
-        })
-        .catch((err) => {
-            console.log(err);
-            return callback(err);
-        });
-};
+const ShortUrl = mongoose.model("veryshortUrl", urlSchema);
 
-const getDB = () => dbConnection;
+module.exports = { ShortUrl };
 
-module.exports = { connectToDB, getDB };
+// const { MongoClient } = require("mongodb");
+
+// let dbConnection;
+
+// const connectToDB = (connection_string, callback) => {
+//     MongoClient.connect(connection_string)
+//         .then((client) => {
+//             dbConnection = client.db();
+//             return callback();
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//             return callback(err);
+//         });
+// };
+
+// const getDB = () => dbConnection;
+
+// module.exports = { connectToDB, getDB };
--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const express = require("express");
-const { connectToDB, getDB } = require("./db");
+// const { connectToDB, getDB } = require("./db");
+const { ShortUrl } = require("./db");
 const { makeid } = require("./id");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // connect mongoDB
-const dbURI =
-    "mongodb+srv://arafat:120338@node-prac.e5v4exj.mongodb.net/Node-Prac?retryWrites=true&w=majority&appName=node-prac";
+// const dbURI = "mongodb+srv://arafat:120338@node-prac.e5v4exj.mongodb.net/Node-Prac?retryWrites=true&w=majority&appName=node-prac";
+const dbURI = "mongodb://127.0.0.1:27017/short-Url";
 
-let db;
-
-connectToDB(dbURI, (err) => {
-    if (!err) {
-        console.log("Connected to db");
-        db = getDB();
+// connectToDB(dbURI, (err) => {
+//     if (!err) {
+//         console.log("Connected to db");
+//         db = getDB();
+//         app.listen(3000, () => {
+//             console.log("listening on port 3000...");
+//         });
+//     } else {
+//         console.log(err);
+//     }
+// });
+mongoose
+    .connect(dbURI)
+    .then(() => {
+        console.log("MongoDB Connected");
         app.listen(3000, () => {
             console.log("listening on port 3000...");
         });
-    } else {
-        console.log(err);
-    }
-});
+    })
+    .catch((err) => console.log(err));
 
 app.set("view engine", "ejs");
 app.use("/public", express.static("./public"));
@@ -28,6 +37,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     // res.sendFile("./views/index.html", { root: __dirname });
+    ShortUrl.find().then((docs) => console.log(docs));
     res.render("index");
 });
 
@@ -39,21 +49,27 @@ app.post("/longurl", (req, res) => {
         urlHash,
     };
 
-    db.collection("url")
-        .insertOne(urlDoc)
-        .then((result) => {
-            // console.log(result);
-            res.json(urlDoc);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    const shortUrl = new ShortUrl(urlDoc);
+
+    shortUrl
+        .save()
+        .then((result) => res.send(result))
+        .catch((err) => res.send(err));
+
+    // db.collection("url")
+    //     .insertOne(urlDoc)
+    //     .then((result) => {
+    //         // console.log(result);
+    //         res.json(urlDoc);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
 });
 
 app.get("/:urlId", (req, res) => {
     const urlId = req.params.urlId;
-    db.collection("url")
-        .findOne({ urlHash: urlId })
+    ShortUrl.findOne({ urlHash: urlId })
         .then((doc) => {
             console.log(doc);
             res.render("redirect", { url: doc.url });
